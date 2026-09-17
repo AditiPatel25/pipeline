08a5a73faf9c0618782c355f0b738e82436db1e7
aditipatel <pate1452@purdue.edu>
2026-09-17 14:19:53 -0500

    fixed cookie settings locally

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -17,7 +17,7 @@ function issueAuthCookie(res: Response, userId: number) {
     res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'none',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 }
